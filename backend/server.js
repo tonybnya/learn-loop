fbe5660fe10e73fdd4d6ccdbbbb7959b96ebc3da
tonybnya <nya.tony2010@gmail.com>
2024-11-04 18:59:17 +0100

    perf: customize the API logger middleware

--- backend/server.js
+++ backend/server.js
@@ -43,3 +43,27 @@ const logger = winston.createLogger({
     }),
   ],
 });
+
+app.use(
+  morgan(":method :url :status :response-time ms - :res[content-length]")
+);
+
+// custom API Logger Middleware
+const apiLogger = (req, res, next) => {
+  const start = Date.now();
+  res.on("finish", () => {
+    const duration = Date.now() - start;
+    logger.info({
+      method: req.method,
+      path: req.path,
+      status: res.statusCode,
+      duration: `${duration}ms`,
+      params: req.params,
+      query: req.query,
+      body: req.method !== "GET" ? req.body : undefined,
+    });
+  });
+  next();
+};
+
+app.use(apiLogger);
