4f06fc78396fa1a99a805479b160875e4901c717
tonybnya <nya.tony2010@gmail.com>
2024-11-05 15:06:20 +0100

    docs: adjust the two last comments

--- backend/server.js
+++ backend/server.js
@@ -469,9 +469,10 @@ function formatUptime(seconds) {
 }
 
 
-// start the server
+// define the PORT for the server
 const PORT = process.env.PORT || 3000;
 
+// start the server
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 })
