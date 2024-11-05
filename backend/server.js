56512efdd9d323e8f58f8dc94146483d9da41803
tonybnya <nya.tony2010@gmail.com>
2024-11-05 15:03:46 +0100

    feat: add the block of code to start the server

--- backend/server.js
+++ backend/server.js
@@ -467,3 +467,11 @@ function formatUptime(seconds) {
 
     return parts.join(' ');
 }
+
+
+// start the server
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
